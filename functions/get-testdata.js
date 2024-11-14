2592fb7832a9c8f7fdbd68babad97ef35672b683
Jianguo Chen <jianguochen@yahoo.com>
2024-11-13 18:09:27 -0800

    add a function to fetch all test data

---
+++ functions/get-testdata.js
@@ -0,0 +1,16 @@
+const config = require('../shared/config');
+
+exports.handler = (event, context, callback) => {
+
+  response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json', // Ensure the response is JSON
+    },
+    body: JSON.stringify(
+      {testdata: config.accounts}
+    )
+  };
+
+  return callback(null, response);
+};
---
+++ verify_account.sh
@@ -0,0 +1,3 @@
+curl -X POST https://visionary-nasturtium-8059cd.netlify.app/.netlify/functions/verify-account \
+  -H "Content-Type: application/json" \
+  -d '{"accountId": "1111a", "pin": "11111"}'
