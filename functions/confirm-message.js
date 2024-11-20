c3ae64ccc99e8766a11117926baaee89d646dea0
Jianguo Chen <jianguochen@yahoo.com>
2024-11-20 13:28:03 -0800

    add option to confirm all messages

--- functions/confirm-message.js
+++ functions/confirm-message.js
@@ -13,10 +13,10 @@ exports.handler = (event, context, callback) => {
     const data = JSON.parse(event.body);
     const {accountId, messageId} = data;
 
-    if (!accountId || !messageId) {
+    if (!accountId) {
       response.statusCode = 400;
       response.body = JSON.stringify(
-        { message: 'missing accountId or messageId'});
+        { message: 'missing accountId'});
       return callback(null, response);
     }
 
@@ -25,30 +25,44 @@ exports.handler = (event, context, callback) => {
       acc => acc.accountId === accountId);
 
     if (account) {
-      // Find the message and remove it
-      const index = account.messages.findIndex(
-        msg => msg.id === String(messageId));
-      if (index !== -1) {
-        account.messages.splice(index, 1);
-        response.body = JSON.stringify(
-          { messages: account.messages });
+      // If messageId is empty, remove all messages
+      if (!messageId) {
+        account.messages = [];
       } else {
-        response.statusCode = 401;
-        response.body = JSON.stringify(
-          { messages: 'Message does not exist for this account' });
+        // Find the message and remove it
+        const index = account.messages.findIndex(
+          msg => msg.id === String(messageId));
+        if (index !== -1) {
+          account.messages.splice(index, 1);
+          response.body = JSON.stringify(
+            {
+              messages: account.messages
+            });
+        } else {
+          response.statusCode = 401;
+          response.body = JSON.stringify(
+            {
+              messages: 'Message does not exist for this account'
+            });
+        }
       }
     } else {
       response.statusCode = 401;
       response.body = JSON.stringify(
-        { message: 'Test account does not exist' });
+        {
+          message: 'Test account does not exist'
+        });
     }
 
     return callback(null, response);
   } catch (error) {
     response.statusCode = 500;
     response.body = JSON.stringify(
-      { message: 'Internal error',
-        error: error.message });
+      {
+        message: 'Internal error',
+        error: error.message,
+        data: event.body
+      });
 
     return callback(null, response);
   }
