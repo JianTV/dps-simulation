15c06d47fe6a5207a38b17a9a1ef5795d45d603f
Jianguo Chen <jianguochen@yahoo.com>
2024-12-31 20:25:41 -0800

    haev a dps.sh

--- get-caller-setting.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST https://visionary-nasturtium-8059cd.netlify.app/.netlify/functions/get-caller-setting \
-  -H "Content-Type: application/json" \
-  -d '{"phone_number": "+16502768941"}'
