ef0bf336d6dd8be1c5899138ee1b2c85bb19707f
Jian <jianguo@tv-messenger.com>
2025-03-01 20:36:08 -0800

    Update get-current-time.js

--- functions/get-current-time.js
+++ functions/get-current-time.js
@@ -1,6 +1,9 @@
 exports.handler = (event, context, callback) => {
     const response = {
       statusCode: 200,
+      headers: {
+         'Content-Type': 'application/json', // Ensure the response is JSON
+      },
       body: JSON.stringify({
         currentTime: Math.floor(Date.now() / 1000),
       }),
