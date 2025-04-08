9fdf5dc5967d759fb7f22041d4ced352ee07c9f3
Jianguo <jianguo@tv-messenger.com>
2025-04-08 12:31:08 -0700

    add a json check endpoint

--- functions/json-check.js
+++ functions/json-check.js
@@ -10,64 +10,9 @@ exports.handler = (event, context, callback) => {
   };
 
   try {
+    console.log("Received json body:", event.body);
+
     const data = JSON.parse(event.body);
-    const {accountId, messageId} = data;
-
-    if (!accountId) {
-      response.statusCode = 400;
-      response.body = JSON.stringify(
-        {
-          message: 'missing accountId',
-          data: event.body
-        });
-      return callback(null, response);
-    }
-
-    // Find the account in test accounts in the config file.
-    const account = config.find(
-      acc => acc.accountId === accountId);
-
-    // Just log the request
-    response.body = JSON.stringify(
-        {
-          messageId: messageId
-        });
-    /*
-    if (account) {
-      // If messageId is empty, remove all messages
-      if (!messageId) {
-        account.messages = [];
-        response.body = JSON.stringify(
-          {
-            messages: account.messages
-          });
-      } else {
-        // Find the message and remove it
-        const index = account.messages.findIndex(
-          msg => msg.id === String(messageId));
-        if (index !== -1) {
-          account.messages.splice(index, 1);
-          response.body = JSON.stringify(
-            {
-              messages: account.messages
-            });
-        } else {
-          response.statusCode = 401;
-          response.body = JSON.stringify(
-            {
-              error: "message id does not exist",
-              data: event.body
-            });
-        }
-      }
-    } else {
-      response.statusCode = 401;
-      response.body = JSON.stringify(
-        {
-          error: 'Test account does not exist'
-        });
-    }
-    */
     return callback(null, response);
   } catch (error) {
     response.statusCode = 500;
